c677536b8fe51f1cf9af562bd4b4dab09325950e
Anondobazar <mstsiddikabegum99@gmail.com>
2026-08-13 21:55:42 +0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// ==========================================
+// BARAKORA - FIREBASE CONFIG
+// ==========================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBv2WY7vqBVfEDUGQZUnvXXzQJdKGi7-PQ",
+  authDomain: "barakora-60846.firebaseapp.com",
+  projectId: "barakora-60846",
+  storageBucket: "barakora-60846.firebasestorage.app",
+  messagingSenderId: "631054410944",
+  appId: "1:631054410944:web:f08f0ffe783820f0027925"
+};
+
+export { firebaseConfig };
